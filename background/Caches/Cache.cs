26691fefd2d2c0284fd69cc60ad9e132483bcaba
yuruisoft <417853832@qq.com>
2018-04-23 18:29:45 +0800

    add GetCacheKeys,so it must be reflected.

--- background/Caches/Cache.cs
+++ background/Caches/Cache.cs
@@ -3,9 +3,12 @@ using Microsoft.Extensions.Caching.Redis;
 using Newtonsoft.Json;
 using StackExchange.Redis;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace background.Caches
@@ -38,6 +41,19 @@ namespace background.Caches
             {
                 memoryCache = value;
             }
+        }
+        public static List<string> GetCacheKeys()
+        {
+            const BindingFlags flags = BindingFlags.Instance | BindingFlags.NonPublic;
+            var entries = memoryCache.GetType().GetField("_entries", flags).GetValue(memoryCache);
+            var cacheItems = entries as IDictionary;
+            var keys = new List<string>();
+            if (cacheItems == null) return keys;
+            foreach (DictionaryEntry cacheItem in cacheItems)
+            {
+                keys.Add(cacheItem.Key.ToString());
+            }
+            return keys;
         }
     }
 
@@ -345,6 +361,50 @@ namespace background.Caches
         }
 
         #endregion
+
+        /// <summary>
+        /// 删除匹配到的缓存
+        /// </summary>
+        /// <param name="pattern"></param>
+        /// <returns></returns>
+        public void RemoveCacheRegex(string pattern)
+        {
+            IList<string> l = SearchCacheRegex(pattern);
+            foreach (var s in l)
+            {
+                Remove(s);
+            }
+        }
+
+        /// <summary>
+        /// 搜索 匹配到的缓存
+        /// </summary>
+        /// <param name="pattern"></param>
+        /// <returns></returns>
+        public IList<string> SearchCacheRegex(string pattern)
+        {
+            var cacheKeys = GetCacheKeys();
+            var l = cacheKeys.Where(k => Regex.IsMatch(k, pattern)).ToList();
+            return l.AsReadOnly();
+        }
+
+        /// <summary>
+        /// 获取所有缓存键
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetCacheKeys()
+        {
+            const BindingFlags flags = BindingFlags.Instance | BindingFlags.NonPublic;
+            var entries = _cache.GetType().GetField("_entries", flags).GetValue(_cache);
+            var cacheItems = entries as IDictionary;
+            var keys = new List<string>();
+            if (cacheItems == null) return keys;
+            foreach (DictionaryEntry cacheItem in cacheItems)
+            {
+                keys.Add(cacheItem.Key.ToString());
+            }
+            return keys;
+        }
 
     }
 
