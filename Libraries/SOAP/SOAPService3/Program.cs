0b71bcfd615f893749966b427b87be5ccbf2de3f
yuruisoft <417853832@qq.com>
2018-04-25 13:40:31 +0800

    add Libraries folder and wexin , SOAP

---
+++ Libraries/SOAP/SOAPService3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.ServiceModel;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CustomMiddleware;
+using System.IO;
+
+namespace SOAPService3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                       .UseKestrel()
+                       .UseContentRoot(Directory.GetCurrentDirectory())
+                       .UseIISIntegration()
+                       .UseStartup<Startup>()
+                       .Build();
+            host.Run();
+        }
+    }
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<CalculatorService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {         
+            //加入一个/CalculatorService.svc 地址，绑定Http
+            app.UseSOAPMiddleware<CalculatorService>("/CalculatorService.svc", new BasicHttpBinding());
+        }
+    }
+
+    public class CalculatorService : ICalculatorService
+    {
+        public double Add(double x, double y) => x + y;
+        public double Divide(double x, double y) => x / y;
+        public double Multiply(double x, double y) => x * y;
+        public double Subtract(double x, double y) => x - y;
+        public string Get(string str) => $"{str} Hello World!";
+    }
+
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        double Add(double x, double y);
+        [OperationContract]
+        double Subtract(double x, double y);
+        [OperationContract]
+        double Multiply(double x, double y);
+        [OperationContract]
+        double Divide(double x, double y);
+
+        [OperationContract]
+        string Get(string str);
+    }
+}
